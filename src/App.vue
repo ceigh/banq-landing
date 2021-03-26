d37e049e05f4b360424383bdcf6a22e86ba661ee
Artjom Löbsack <me@ceigh.com>
2021-03-26 20:52:51 +0500

    feat: add header template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    <SectionStart />
-  </div>
+  <Header />
+  <SectionStart />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+
+import Header from './components/Header.vue'
 import SectionStart from './components/section/Start.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    Header,
     SectionStart
   }
 })
