19d9a1d705588a3a3a9447046766a057bcf65685
Artjom Löbsack <me@ceigh.com>
2021-03-28 18:36:00 +0500

    feat(section): add properly section template

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <Header />
 <SectionStart />
 <SectionUsual />
+<SectionProperly />
 </template>
 
 <script lang='ts'>
@@ -10,12 +11,14 @@ import { defineComponent } from 'vue'
 import Header from './components/Header.vue'
 import SectionStart from './components/section/Start.vue'
 import SectionUsual from './components/section/Usual.vue'
+import SectionProperly from './components/section/Properly.vue'
 
 export default defineComponent({
   components: {
     Header,
     SectionStart,
-    SectionUsual
+    SectionUsual,
+    SectionProperly
   }
 })
 </script>
---
+++ src/components/section/Properly.vue
@@ -0,0 +1,30 @@
+<template>
+<div class='container'>
+  <img class='arrow' src='@/assets/img/section/usual/arrow.svg'
+    alt='arrow' />
+
+  <div class='content'>
+  </div>
+</div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+  position: relative;
+  margin-top: $indent-half;
+  padding-top: 2 * $indent;
+}
+
+.arrow {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 3 * $indent;
+}
+
+.content {
+  background: blue;
+  height: 5rem;
+}
+</style>
