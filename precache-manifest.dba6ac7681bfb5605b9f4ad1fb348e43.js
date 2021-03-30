7bc92c1f720ce6ac0b721f09116459e823681d0e
ceigh <ceigh@users.noreply.github.com>
2021-03-30 17:08:29 +0000

    deploy: 525db47f8935523a447c39b469f483df19687ca0

---
+++ precache-manifest.dba6ac7681bfb5605b9f4ad1fb348e43.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faca40e03571f8762c90",
+    "url": "/banq-landing/css/app.e09184e6.css"
+  },
+  {
+    "revision": "0c8ea4237f9913f26ccf9b914497674f",
+    "url": "/banq-landing/fonts/SFUIDisplay-bold.woff"
+  },
+  {
+    "revision": "78e4a00e181a0c4e4998750a31e8dde5",
+    "url": "/banq-landing/fonts/SFUIDisplay-normal.woff"
+  },
+  {
+    "revision": "1ea7ca94eb4aea7c1987caed6b406d3f",
+    "url": "/banq-landing/img/1.1ea7ca94.png"
+  },
+  {
+    "revision": "e1080dfe0290eb3ae863c66c2d0c88a3",
+    "url": "/banq-landing/img/1.e1080dfe.png"
+  },
+  {
+    "revision": "0cf822df0d6b0c673031bab53403cd62",
+    "url": "/banq-landing/img/2.0cf822df.png"
+  },
+  {
+    "revision": "1f863ac78817425a49ebf90a7c81bc04",
+    "url": "/banq-landing/img/3.1f863ac7.png"
+  },
+  {
+    "revision": "98f0728f946481b815c69d539bb64076",
+    "url": "/banq-landing/img/4.98f0728f.png"
+  },
+  {
+    "revision": "c4fdb9a4fc878493675dd40e17f5b8a8",
+    "url": "/banq-landing/img/arrow.c4fdb9a4.svg"
+  },
+  {
+    "revision": "53af343363a010a5816622ed125c3d68",
+    "url": "/banq-landing/img/bracket.53af3433.svg"
+  },
+  {
+    "revision": "39e4dbbf8c862247f41aff311ee065e7",
+    "url": "/banq-landing/img/card1.39e4dbbf.png"
+  },
+  {
+    "revision": "2945ec6634715fb292942caad1a3cb79",
+    "url": "/banq-landing/img/card2.2945ec66.png"
+  },
+  {
+    "revision": "1952ecd942a46d4f28f4dd604f5d3740",
+    "url": "/banq-landing/img/card3.1952ecd9.png"
+  },
+  {
+    "revision": "76e9de190b40898d139c9260949bf132",
+    "url": "/banq-landing/img/logo.76e9de19.svg"
+  },
+  {
+    "revision": "750709fc8c51da9b4543318c280e5748",
+    "url": "/banq-landing/index.html"
+  },
+  {
+    "revision": "faca40e03571f8762c90",
+    "url": "/banq-landing/js/app.57bbabf3.js"
+  },
+  {
+    "revision": "c9ea9b43e7b5fed67b70",
+    "url": "/banq-landing/js/chunk-vendors.09a77241.js"
+  },
+  {
+    "revision": "4cd749ccb3bb19259406acb9d6329c06",
+    "url": "/banq-landing/manifest.json"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/banq-landing/precache-manifest.dba6ac7681bfb5605b9f4ad1fb348e43.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "banq-landing"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
