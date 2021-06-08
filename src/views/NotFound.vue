e88fa7127af99305bcbc80e12b4b73219ee42a90
Artjom Löbsack <me@ceigh.com>
2021-06-08 19:14:41 +0500

    feat(not found): add content

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,43 @@
 <template>
-  <p>not found</p>
+<div class='container'>
+  <h1>Страница не найдена.</h1>
+</div>
 </template>
+
+<style lang='scss' scoped>
+.container {
+  --padding-v: #{1.4 * $indent-4};
+  --padding-h: #{$indent-2};
+
+  padding: var(--padding-v) var(--padding-h);
+
+  @include lg {
+    --padding-v: #{1.7 * $indent-4};
+  }
+
+  @include md {
+    --padding-h: #{$indent};
+  }
+
+  @include sm {
+    --padding-h: #{$indent-half};
+  }
+}
+
+h1 {
+  font-size: 4rem;
+  text-align: center;
+
+  @include lg {
+    font-size: 3rem;
+  }
+
+  @include md {
+    font-size: 2.5rem;
+  }
+
+  @include sm {
+    font-size: 1.5rem;
+  }
+}
+</style>
